99325575fda8fad620b1cabfe7fdc99abcbb0438
alexsdixon <aledix22@yahoo.com>
2018-07-12 15:22:16 -0500

    Created project and added dependencies and MySQL database connection

---
+++ src/main/java/net/alexdixon/soccercompetitiontracker/SoccerCompetitionTrackerApplication.java
@@ -0,0 +1,12 @@
+package net.alexdixon.soccercompetitiontracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoccerCompetitionTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoccerCompetitionTrackerApplication.class, args);
+	}
+}
